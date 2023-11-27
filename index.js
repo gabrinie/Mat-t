62dc8ef6e62b9d27d09659a241ba696424efe496
gabrinie <gabrielafranco83@gmail.com>
2023-11-26 22:38:59 -0400

    1st

---
+++ index.js
@@ -0,0 +1,54 @@
+function calculateHeight(distance) {
+    var height = -Math.pow(distance, 2) + (3 * distance); // y = -x² + 3x  
+
+    if(height < 0){
+        height = height*-1;
+    }
+    return height;
+}
+
+
+function calculateAndDisplay() {
+    let distanceInput = document.getElementById('distance');
+
+    let distance = parseFloat(distanceInput.value);
+
+    if (!isNaN(distance) && distance>0) {
+        let height = calculateHeight(distance);
+
+        let resultsDiv = document.querySelector('.results');
+        resultsDiv.innerHTML = `<h2 class="result-title">Em ${distance}km, a altura do seu Foguete será:</h2>
+        <h2 class='height'> ${height} km!</h2>`;
+
+        let sceneDiv = document.querySelector('.scene');
+        sceneDiv.innerHTML = '<div class="rocket"><img src="./assets/rokcet.png" alt=""></div>'
+        stars()
+
+    } else {
+        
+        let resultsDiv = document.querySelector('.results');
+        resultsDiv.innerHTML = '<p class="error-message">Por favor, insira um número válido.</p>';
+    }
+}
+
+function stars() {
+    let count = 60;
+    let scene = document.querySelector(".scene");
+    let i = 0;
+    while(i < count){
+        let star = document.createElement("i");
+        let x = Math.floor(Math.random() * window.innerWidth);
+
+        let duration = Math.random() * 1;
+        let h = Math.random() * 100;
+
+        star.style.left = x+ 'px';
+        star.style.width = 1 + 'px';
+        star.style.height = 60 + h + 'px';
+        star.style.animationDuration = duration + 's';
+
+        scene.appendChild(star);
+        i++;
+    }
+
+}
